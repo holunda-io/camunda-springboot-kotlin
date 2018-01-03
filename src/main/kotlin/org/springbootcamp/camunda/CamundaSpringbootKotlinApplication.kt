b3bcb4856ec35291e7b573b11e5f4ceb0657e7a0
Jan Galinski <jan.galinski@holisticon.de>
2018-01-03 15:51:43 +0100

    initial from start.spring.io

---
+++ src/main/kotlin/org/springbootcamp/camunda/CamundaSpringbootKotlinApplication.kt
@@ -0,0 +1,11 @@
+package org.springbootcamp.camunda
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CamundaSpringbootKotlinApplication
+
+fun main(args: Array<String>) {
+    runApplication<CamundaSpringbootKotlinApplication>(*args)
+}
